022bce8464eb9c2c65e182f97f45c35d3d378423
kexy964 <134679188+kexy964@users.noreply.github.com>
2024-04-10 15:53:22 +0800

    Create 8.counter.php

---
+++ 8.counter.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    if (!isset($_SESSION["counter"]))
+        $_SESSION["counter"]=1;
+    else
+        $_SESSION["counter"]++;
+
+    echo "counter=".$_SESSION["counter"];
+    echo "<br><a href=9.reset_counter.php>重置counter</a>";
+?>
+
+要讀取/操作session變數之前，需呼叫session_start()，才能夠跨網頁
+$_SESSION['變數'] = 值。設定Session變數
+isset($_SESSION['變數'])
